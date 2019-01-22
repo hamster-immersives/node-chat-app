cdbf7ca2b93574da0e64d385ec69158b6f4268f0
pak <pak@1916.lan>
2019-01-22 15:19:47 -0500

    second

--- socket/socket.js
+++ socket/socket.js
@@ -1,8 +1,23 @@
 function socket(io) {
 
     io.on('connection', function(socket) {
-        console.log('a user has connected');
-    })
+        console.log('a user has connected', socket.id);
+
+        socket.emit('connected', 'you are connected')
+
+        socket.on('new message', function(msg) {
+            const newDate = new Date();
+            var data = {
+                message: msg.message,
+                username: msg.username,
+                date: newDate.toLocaleTimeString()
+            }
+            socket.emit('chat message', data);
+        })
+        socket.on('disconnect', () => {
+            console.log(`user ${socket.id} has disconnected`);
+        })
+    });
 }
 
 module.exports = socket;
